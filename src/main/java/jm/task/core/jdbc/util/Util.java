3f15977d3eb05444bb97ab1fec302936dd73a355
Igorkz3 <kazachenko.goga@inbox.ru>
2025-08-23 01:42:38 +0300

    jdbc

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+   private static final String URL = "jdbc:mysql://localhost:3306/project_schema ";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Соединение с базой данных установлено!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Ошибка подключения к базе данных: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
+
+
